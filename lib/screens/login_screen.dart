da7b798efde78d1e4d9ea9576ce7614dec7e33a6
Hanrocky <hanrocky.halim@gmail.com>
2023-11-28 23:54:26 +0700

    Added login UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram/utils/colors.dart';
+import 'package:flutter_instagram/widgets/text_field_input.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,7 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
@@ -19,7 +29,10 @@ class _LoginScreenState extends State<LoginScreen> {
         width: double.infinity,
         child: Column(
           children: [
-            Flexible(child: Container(), flex: 2,),
+            Flexible(
+              child: Container(),
+              flex: 1,
+            ),
             // Svg Image
             SvgPicture.asset('assets/ic_instagram.svg',
                 colorFilter: ColorFilter.mode(primaryColor, BlendMode.srcIn),
@@ -28,10 +41,67 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 64,
             ),
             // Text field input for email
-
+            TextFieldInput(
+              textEditingController: _emailController,
+              textInputType: TextInputType.emailAddress,
+              hintText: "Enter your email",
+            ),
+            const SizedBox(
+              height: 24,
+            ),
             // Text field input for password
+            TextFieldInput(
+              textEditingController: _passwordController,
+              textInputType: TextInputType.text,
+              hintText: "Enter your password",
+              isPassword: true,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
             // Login Button
+            InkWell(
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  color: blueColor,
+                ),
+                child: const Text('Log in'),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(),
+            ),
             // Signup
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: const Text("Don't have an account?"),
+                ),
+                GestureDetector(
+                  onTap: (){},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       )),
